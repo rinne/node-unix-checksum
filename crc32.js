c8e6654f622dbfbbb43384557070f49690eb36cc
Timo J. Rinne <tri@iki.fi>
2019-11-10 11:57:10 +0000

    Canonicalize crc32 and crc32c contexts after final().

--- crc32.js
+++ crc32.js
@@ -3,19 +3,28 @@
 const CRC32Generator = require('./crc32generator.js');
 
 var CRC32 = function() {
+	this.dead = false;
 	this.length = 0;
 	this.crc = new CRC32Generator(-306674912);
 }
 
 CRC32.prototype.update = function(b) {
+	if (this.dead) {
+		throw new Error('Checksum context in error state');
+	}
 	this.crc.update(b);
 	this.length = this.crc.length;
 	return this;
 }
 
 CRC32.prototype.final = function(encoding) {
+	if (this.dead) {
+		throw new Error('Checksum context in error state');
+	}
 	var r = this.crc.final(encoding);
 	this.state = this.crc.state;
+	delete this.crc;
+	this.dead = true;
 	return r;
 }
 
--- crc32c.js
+++ crc32c.js
@@ -3,19 +3,28 @@
 const CRC32Generator = require('./crc32generator.js');
 
 var CRC32C = function() {
+	this.dead = false;
 	this.length = 0;
 	this.crc = new CRC32Generator(-2097792136);
 }
 
 CRC32C.prototype.update = function(b) {
+	if (this.dead) {
+		throw new Error('Checksum context in error state');
+	}
 	this.crc.update(b);
 	this.length = this.crc.length;
 	return this;
 }
 
 CRC32C.prototype.final = function(encoding) {
+	if (this.dead) {
+		throw new Error('Checksum context in error state');
+	}
 	var r = this.crc.final(encoding);
 	this.state = this.crc.state;
+	delete this.crc;
+	this.dead = true;
 	return r;
 }
 
